daf76119db96e2b252ab1e3bb68829c388c294ae
illiaBabak <illiababak776@gmail.com>
2023-08-28 17:00:27 +0200

    Snake
    Building a project

---
+++ app/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.scss';
+
+function App() {
+
+  return (
+    <div className='container'>
+      <div className='overlay-start'></div>
+    </div>
+  );
+}
+export default App;
---
+++ app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
