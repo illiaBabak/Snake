bbd3f250f2ce3f31092640e6fb17229b09a9121d
illiaBabak <illiababak776@gmail.com>
2023-08-28 18:32:53 +0200

    Snake
    Added graphics files for the game and created a start menu with a header, game image and start button

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,7 +4,15 @@ function App() {
 
   return (
     <div className='container'>
-      <div className='overlay-start'></div>
+      <div className='overlay-start'>
+        <div className='header'><p>Snake game</p></div>
+        <div className='img-block'><img src='https://www.google.com/logos/fnbx/snake_arcade/cta_alt.png'></img></div>
+        <div className='start-button'>Start</div>
+      </div>
+
+      <div className='main hidden'>
+
+      </div>
     </div>
   );
 }
